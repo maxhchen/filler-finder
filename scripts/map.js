b9d42f5a0c5c708d42fff994799a2370626c0c8c
Tom Tan <ttan1465@gmail.com>
2018-07-30 10:19:39 -0700

    added map.js, changes to style.css, changes to home.html

--- scripts/map.js
+++ scripts/map.js
@@ -1,1 +1,11 @@
 //API key = AIzaSyADULoh4vZX2MsZs4SAgpTgOaXPbjsCNBA
+let API_KEY = 'AIzaSyADULoh4vZX2MsZs4SAgpTgOaXPbjsCNBA';
+
+function initMap() {
+  let location = {lat: -200, lng: 200};
+
+  let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: location,
+    });
+}
