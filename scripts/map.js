6fd877afeebd0275c8543053cf9c637d7cd84b1f
Tom Tan <ttan1465@gmail.com>
2018-07-30 10:37:13 -0700

    Added comments and marker

--- scripts/map.js
+++ scripts/map.js
@@ -2,9 +2,15 @@
 let API_KEY = 'AIzaSyADULoh4vZX2MsZs4SAgpTgOaXPbjsCNBA';
 
 function initMap() {
+  //Locations
   let location = {lat: 37.403619, lng: -122.031625};
+
+  //The Map
   let map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
       center: location,
     });
+
+  //Markers
+  let testMarker = new google.maps.Marker({position: location, map: map});
 }
