41200d4a376971cee46fbd1104e11e5f5ef00ee8
Tom Tan <ttan1465@gmail.com>
2018-07-30 12:09:36 -0700

    Added the feature where person can enter their address and it will move the map

--- scripts/map.js
+++ scripts/map.js
@@ -1,8 +1,9 @@
 //API key = AIzaSyADULoh4vZX2MsZs4SAgpTgOaXPbjsCNBA
 let API_KEY = 'AIzaSyADULoh4vZX2MsZs4SAgpTgOaXPbjsCNBA';
+let geocoder;
 
 function initMap() {
-
+  geocoder = new google.maps.Geocoder();
   //Locations
   let location = {lat: 37.403619, lng: -122.031625};
 
@@ -32,4 +33,28 @@ location_list.forEach(function(feature) {
     map: map,
   });
 });
+
+//Give the button an event listener so that when clicked it will run codeAddress()
+document.querySelector('#enterAddress').addEventListener('click', e=> {
+  codeAddress(geocoder, map);
+  });
+}
+
+//Takes the address from the search bar and centers map to that location
+function codeAddress(geocoder, map) {
+  let address = document.querySelector('#address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+
+        //This will place a marker on the address entered
+        // let marker = new google.maps.Marker({
+        //     map: map,
+        //     position: results[0].geometry.location
+        // });
+
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
 }
