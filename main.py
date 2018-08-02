a476512ad588f86cbb182e33738933884fe4285e
Tom Tan <ttan1465@gmail.com>
2018-08-02 11:26:35 -0700

    Added back which person entered email

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ class Filler(ndb.Model):
 
 class APIkey(ndb.Model):
     key = ndb.StringProperty()
-    
+
 ######################################################################
 
 class HomePage(webapp2.RequestHandler):
@@ -177,7 +177,7 @@ class AddFiller(webapp2.RequestHandler):
             current_filler = Filler.query().filter(Filler.location == location).get()
 
             if not current_filler:
-                current_filler = Filler(name = name, location = location, type = type, description = description, company = company)
+                current_filler = Filler(name = name, location = location, type = type, description = description, company = company, current_user_email = current_user.email())
                 current_filler.put()
                 current_person.limit += 1
                 current_person.put()
